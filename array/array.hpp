179ee188851b56e634ee49ecda6ac9af50ca1317
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-20 17:26:42 -0500

    started

---
+++ array/array.hpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+
+template <typename T> class Array {
+  T *arrptr;
+  T *iptr;
+  std::size_t msize;
+  std::size_t mcapacity;
+
+public:
+  Array();
+  Array(std::size_t capacity);
+  T get(int index) const;
+  void modify(int index, T elem);
+  T add(T elem);
+  void remove(T elem);
+  T insert(T elem, int index);
+  std::size_t size() const;
+  std::size_t capacity() const;
+  void print() const;
+};
+
+
+template <typename T>
+Array<T>::Array(std::size_t capacity)
+    : arrptr(new T[capacity]), iptr(arrptr), msize(0), mcapacity(capacity) {}
+
+template <typename T>
+Array<T>::Array():Array(10) {}
