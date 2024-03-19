59975efb39fc9f0e856f20bc6a423d752e44b777
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-19 16:44:55 -0400

    works w. resize

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,19 +1,7 @@
 #include "vector.hpp"
-#include <cstdio>
 
 int main() {
-  vector<int> v(2);
-  v.append(5);
-  v.append(5);
-  v.append(5);
-  v.insert(3, 1);
-  v.print();
-  printf("Count of %d: %d\n", 5, v.count(5));
-  v.remove(5, 2);
-  v.print();
-  printf("Size: %ld\n", v.size());
-  printf("Count of %d: %d\n", 5, v.count(5));
-  v.clear();
+  vector<int> v;
   v.print();
   return 0;
 }
